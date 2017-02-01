e6b2a158972466e582d76869886fa7b21cc6675a
Tommy Lindman <tomli962@student.liu.se>
2017-02-01 14:22:31 +0100

    All tests for lab 2 passed.

--- redovisning/espy/Interpreter/Property.py
+++ redovisning/espy/Interpreter/Property.py
@@ -5,14 +5,14 @@ class ReadOnlyException(Exception):
   Exception thrown when accessing a read only property
   '''
   def __init__(self):
-    pass
+    super().__init__("ReadOnlyException")
 
 class WriteOnlyException(Exception):
   '''
   Exception thrown when accessing a write only property
   '''
   def __init__(self):
-    pass
+    super().__init__("WriteOnlyException")
 
 class Property:
   '''
@@ -21,28 +21,44 @@ class Property:
   * setter a Function that is called when setting the value
   * this the object to which this property belongs
   '''
-  def __init__(self):
-    pass
+  def __init__(self, this, getter = None, setter = None):
+    self.this = this
+    self.getter = getter
+    self.setter = setter
 
   def get(self):
     '''
     Get the value or raise WriteOnlyException
     '''
-    pass
+    if self.getter == None:
+      raise WriteOnlyException()
+    else:
+      return self.getter(self.this)
     
   def set(self, value):
     '''
     Set the value or raise ReadOnlyException
     '''
-    pass
+    if self.setter == None:
+      raise ReadOnlyException()
+    else:
+      self.setter(self.this, value)
  
   def merge(self, other):
     '''
     Merge two properties.
     '''
-    pass
+    if not other.this == None:
+      self.this = other.this
+    if not other.getter == None:
+      self.getter = other.getter
+    if not other.setter == None:
+      self.setter = other.setter
+
   def clone(self):
     '''
     Clone a property (useful when creating new objects).
     '''
-    pass
+    new_prop = Property(self.this, self.getter, self.setter)
+
+    return new_prop
