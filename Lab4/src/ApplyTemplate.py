31e1719a8e8a14715c623f761f78f7f679413646
Tommy Lindman <tomli962@student.liu.se>
2017-03-20 14:51:15 +0100

    Lab 4, done work.

--- Lab4/src/ApplyTemplate.py
+++ Lab4/src/ApplyTemplate.py
@@ -1,1 +1,31 @@
- 
+#!/usr/bin/env python3
+
+import inspect
+import ast
+
+def apply_template(bs, f_body, rs, f_return):
+    def t(f):
+        f_ast = ast.parse(inspect.getsource(f))
+        f_ast.body[0].decorator_list = []
+
+        body_ast = ast.parse(inspect.getsource(f_body)).body[0]
+        body_node = body_ast.body[0]
+
+        return_ast = ast.parse(inspect.getsource(f_return)).body[0]
+        return_node = return_ast.body[0]
+
+        class T(ast.NodeTransformer):
+            def visit_Expr(self, node):
+                if hasattr(node.value, 'id'):
+                    if (node.value.id == '__body__'):
+                        return body_node
+                    elif (node.value.id == '__return__'):
+                        return return_node
+                    else:
+                        return node
+                else:
+                    return node
+
+        exec(compile(T().visit(f_ast), __file__, mode='exec'))
+        return locals()[f_ast.body[0].name]
+    return t
--- Lab4/src/LogTimingDecorator.py
+++ Lab4/src/LogTimingDecorator.py
@@ -1,12 +1,21 @@
 import time
 
+class FunctionInfo:
+  def __init__(self, func_name, func_args, func_result, start, end):
+    self.func_name = func_name
+    self.func_args = func_args
+    self.func_result = func_result
+    self.start = start
+    self.end = end
+
 class Logger:
   '''
   Logger class. It should contains an array of objects containing the information about function calls
   '''
   
   def __init__(self):
-    pass
+    self.function_calls = []
+
   def log_function_call(self, func_name, func_args, func_result, start, end):
     '''
     Call this function after the function has finished been executed
@@ -16,10 +25,26 @@ class Logger:
     start: time when the function was started
     end:   time when the function has finish execution
     '''
-    pass
+    #print("calling log_function_call")
+    self.function_calls.append(FunctionInfo(func_name, func_args, func_result, start, end))
 
 def logtiming(logger):
   '''
   Decorator that will add an entry in logger after a function call
   '''
-  pass
+  def make_decorator(func):
+    def decorator(*x):
+      #measure start time
+      start = time.time()
+      #call function and save to res variable
+      res = func(*x)
+      #measure end time
+      end = time.time()
+      # store to function_calls
+      logger.log_function_call(func.__name__,x, res, start, end)
+      # return res
+      #print(func.__name__)
+      #print(x)
+      return res
+    return decorator
+  return make_decorator
